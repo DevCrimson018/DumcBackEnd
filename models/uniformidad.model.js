3b668edc75d3ce36269bdf62bf056d694ce9168e
DevCrimson018 <elidev018@gmail.com>
2024-11-29 00:34:30 -0400

    uniformidad Re-estructurada

--- models/uniformidad.model.js
+++ models/uniformidad.model.js
@@ -12,40 +12,124 @@ const disciplinaSchema = new Schema({
         miembrosFemenino: {type: Number, required: true}
     },
     faltas: {
-        galardonesFaltantesCamisa: {type: Number, required: false},
-        bolsillosIncorrectos: {type: Number, required: false},
-        pantalonesConPinza: {type: Number, required: false},
-        pantalonesConTapa: {type: Number, required: false},
-        faldasSinCorteA: {type: Number, required: false},
-        faldasSinPlisadoFrontal: {type: Number, required: false},
-        faldasPorEncimaDeRodillas: {type: Number, required: false},
-        calzadoDeColores: {type: Number, required: false},
-        calzadoInformal: {type: Number, required: false},
-        mediasColorIncorrecto: {type: Number, required: false},
-        alturaDeMediasIncorrecta: {type: Number, required: false},
-        correasNoOficiales: {type: Number, required: false},
-        correasIncorrectas: {type: Number, required: false},
-        boinasFaltantes: {type: Number, required: false},
-        triangulosFaltantes: {type: Number, required: false},
-        globosFaltantes: {type: Number, required: false},
-        arcosFaltantes: {type: Number, required: false},
-        panoletasFaltantes: {type: Number, required: false},
-        anillosFaltantes: {type: Number, required: false},
-        unionesFaltantes: {type: Number, required: false},
-        cordonesDeDirector: {type: Number, required: false},
-        paresDeGalonesDeDirector: {type: Number, required: false},
-        personasInvestidasDeClases: {type: Number, required: false},
-        distintivosDeClase: {type: Number, required: false},
-        botonesDeClase: {type: Number, required: false},
-        rangosDeClase: {type: Number, required: false},
-        personasSinBanda: {type: Number, required: false},
-        bandasSinOctagono: {type: Number, required: false},
-        bandasSinEspecialidades: {type: Number, required: false},
-        bandasConMenosDe4Especialidades: {type: Number, required: false},
-        bandasSinBanderaNacional: {type: Number, required: false},
-        bandasSinDistintivoDeNombre: {type: Number, required: false},
+        camisas: {
+            cantidadGalonesFalsos: {type: Number, required: false},
+            bolsillosIncorrectos: {type: Number, required: false},
+            comentario: {type: String, required: false},
+            puntos: {type: Number, required: false}
+        },
+       
+        pantalones: {
+            cantidadPantalonesConPinza: {type: Number, required: false},
+            cantidadPantalonesConTapa: {type: Number, required: false},
+            comentario: {type: String, required: false},
+            puntos: {type: Number, required: false}
+        },
+        faldas: {
+            cantidadFaldasSinCorteA: {type: Number, required: false},
+            faldasSinPlisadoFrontal: {type: Number, required: false},
+            faldasPorEncimaDeRodillas: {type: Number, required: false},
+            comentario: {type: String, required: false},
+            puntos: {type: Number, required: false}
+        },
+        calzado: {
+            calzadoDeColores: {type: Number, required: false},
+            calzadoInformal: {type: Number, required: false},
+            comentario: {type: String, required: false},
+            puntos: {type: Number, required: false}
+        },
+        medias: {
+            cantidadMediasColorIncorrecto: {type: Number, required: false},
+            alturaDeMediasIncorrecta: {type: Number, required: false},
+            comentario: {type: String, required: false},
+            puntos: {type: Number, required: false}
+        },
+        correas: {
+            cantidadCorreasNoOficiales: {type: Number, required: false},
+            cantidadCorreasIncorrectas: {type: Number, required: false},
+            comentario: {type: String, required: false},
+            puntos: {type: Number, required: false}
+        },
+        boinas: {
+            cantidadBoinasFaltantes: {type: Number, required: false},
+            comentario: {type: String, required: false},
+            puntos: {type: Number, required: false}
+        },
+        insignias: {
+            triangulosFaltantes: {type: Number, required: false},
+            globosFaltantes: {type: Number, required: false},
+            arcosFaltantes: {type: Number, required: false},
+            comentario: {type: String, required: false},
+            puntos: {type: Number, required: false}
+        },
+        panoletas_anillos: {
+            panoletasFaltantes: {type: Number, required: false},
+            anillosFaltantes: {type: Number, required: false},
+            comentario: {type: String, required: false},
+            puntos: {type: Number, required: false}
+        },
+        union: {
+            unionesFaltantes: {type: Number, required: false},
+            comentario: {type: String, required: false},
+            puntos: {type: Number, required: false}
+        },
+        directores: {
+            cantidadCordonesDeDirector: {type: Number, required: false},
+            cantidadParesDeGalonesDeDirector: {type: Number, required: false},
+            comentario: {type: String, required: false},
+            puntos: {type: Number, required: false}
+        },
+
+        cantidadPersonasInvestidasDeClases: {type: Number, required: false},
+        
+        distintivos: {
+            distintivosDeClase: {type: Number, required: false},
+            comentario: {type: String, required: false},
+            puntos: {type: Number, required: false}
+        },
+
+        botones: {
+            botonesDeClase: {type: Number, required: false},
+            comentario: {type: String, required: false},
+            puntos: {type: Number, required: false}
+        },
+        rangos: {
+            rangosDeClase: {type: Number, required: false},
+            comentario: {type: String, required: false},
+            puntos: {type: Number, required: false}
+        },
+        bandas: {
+            personasSinBanda: {type: Number, required: false},
+            bandasSinOctagono: {type: Number, required: false},
+            comentario: {type: String, required: false},
+            puntos: {type: Number, required: false}
+        },
+        
+        especialidades: {
+            bandasSinEspecialidades: {type: Number, required: false},
+            bandasConMenosDe4Especialidades: {type: Number, required: false},
+            comentario: {type: String, required: false},
+            puntos: {type: Number, required: false}
+        },
+        
+        bandera: {
+            bandasSinBanderaNacional: {type: Number, required: false},
+            comentario: {type: String, required: false},
+            puntos: {type: Number, required: false}
+        },
+
+        distitivoBanda: {
+            bandasSinDistintivoDeNombre: {type: Number, required: false},
+            comentario: {type: String, required: false},
+            puntos: {type: Number, required: false}
+        },
+        
 
     },
+
+    puntosGeneral: {type: Number, require: false},
+    comentarioGeneral: {type: String, required: false},
+    
     fecha: {type: String, required: true},
 })
 
